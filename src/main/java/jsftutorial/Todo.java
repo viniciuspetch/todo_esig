f5e5d7d2e7ef4b2bd327f3815f1b05064f2548ac
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-02-12 23:30:05 -0300

    Edit and delete via Hibernate

--- src/main/java/jsftutorial/Todo.java
+++ src/main/java/jsftutorial/Todo.java
@@ -33,6 +33,7 @@ public class Todo {
 	    entityManager.close();
 	    entityManagerFactory.close();
 	    */
+		/*
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("todo");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		List<TodoItem> newList = entityManager.createQuery("select e from TodoItem e").getResultList();
@@ -43,6 +44,7 @@ public class Todo {
 		
 	    entityManager.close();
 	    entityManagerFactory.close();
+	    */
 	}
 
 	public void updateTodoItemList() {		
@@ -58,37 +60,6 @@ public class Todo {
 		
 	    entityManager.close();
 	    entityManagerFactory.close();
-		/*
-		itemList = new ArrayList<TodoItem>();
-
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		String url = "jdbc:postgresql://localhost:5432/todo";
-		Properties props = new Properties();
-		props.setProperty("user", "postgres");
-		props.setProperty("password", "postgres");
-		try {
-			conn = DriverManager.getConnection(url, props);
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM items");
-			while (rs.next()) {
-				//System.out.println(rs.getString(1));
-				//System.out.println(rs.getString(2));
-				TodoItem item = new TodoItem();
-				item.set(rs.getInt(1), rs.getString(2));
-				itemList.add(item);
-			}
-			rs.close();
-			st.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
 	}
 
 	@PostConstruct
@@ -124,41 +95,50 @@ public class Todo {
 	    entityManager.close();
 	    entityManagerFactory.close();
 		
+	    newContent = null;
+	    
 		updateTodoItemList();
 	}
 
 	public void update() {
 		System.out.println("update " + Integer.toString(editId) + " " + editContent);
-		try {
-			PreparedStatement st = conn.prepareStatement("UPDATE items SET content=? WHERE id=?");
-			st.setString(1, editContent);
-			st.setInt(2, editId);
-			int rowsEdited = st.executeUpdate();
-			System.out.println(rowsEdited);
-			st.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		editId = -1;
-		editContent = "";
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("todo");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();		
+		
+		TodoItem updateItem = new TodoItem();
+		updateItem.setId(editId);
+		updateItem.setContent(editContent);
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(updateItem);
+		entityManager.getTransaction().commit();
+		
+	    entityManager.close();
+	    entityManagerFactory.close();
+		
+	    editId = -1;
+		editContent = null;
+		
 		updateTodoItemList();
 	}
 
 	public void delete(int id) {
 		System.out.println("delete " + Integer.toString(id));
-		try {
-			PreparedStatement st = conn.prepareStatement("DELETE FROM items WHERE id = ?");
-			st.setInt(1, id);
-			int rowsDeleted = st.executeUpdate();
-			System.out.println(rowsDeleted);
-			st.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("todo");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();		
+		
+		TodoItem foundItem = entityManager.find(TodoItem.class, id);
+		System.out.println(foundItem.getContent());
+		
+		entityManager.getTransaction().begin();
+		entityManager.remove(foundItem);
+		entityManager.getTransaction().commit();
+		
+	    entityManager.close();
+	    entityManagerFactory.close();
+		
 		updateTodoItemList();
 	}
 	
