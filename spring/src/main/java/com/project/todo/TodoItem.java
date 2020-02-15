eb475bcf33e767e7ff2b0c4062a6817c2cc78e02
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-02-15 15:43:42 -0300

    Check/uncheck functionalities

--- spring/src/main/java/com/project/todo/TodoItem.java
+++ spring/src/main/java/com/project/todo/TodoItem.java
@@ -4,36 +4,40 @@ import javax.persistence.Entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name="items")
+@Table(name = "items")
 public class TodoItem {
 	@Id
 	@GeneratedValue
 	private int id;
 	private String content;
+	private Boolean checked;
 
-	
 	public void set(int id, String content) {
 		this.setId(id);
 		this.setContent(content);
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getContent() {
 		return content;
 	}
 
-
 	public void setContent(String content) {
 		this.content = content;
-	}	
+	}
+
+	public Boolean getChecked() {
+		return checked;
+	}
+
+	public void setChecked(Boolean checked) {
+		this.checked = checked;
+	}
 }
