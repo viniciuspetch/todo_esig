2264686bbdcc4ee78affe3cdf8eb2a99fcb972c3
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-02-16 20:07:02 -0300

    React initial version

---
+++ frontreact/src/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./App.css";
+
+class TodoItemCheck extends React.Component {
+  handleSubmit(event) {
+    console.log("handleSubmit");
+    return false;
+  }
+
+  render() {
+    if (this.props.checked === "true") {
+      return (
+        <form action="#" onSubmit={this.handleSubmit}>
+          <input type="hidden" value={this.props.id} name="id" />
+          <input type="hidden" value={this.props.content} name="content" />
+          <input type="hidden" value={this.props.checked} name="checked" />
+          <input
+            style={{ margin: "5px 0 0 0" }}
+            className="btn btn-sm btn-primary"
+            type="submit"
+            value="Uncheck"
+          />
+        </form>
+      );
+    } else {
+      return (
+        <form action="#" onSubmit={this.handleSubmit}>
+          <input type="hidden" value={this.props.id} name="id" />
+          <input type="hidden" value={this.props.content} name="content" />
+          <input type="hidden" value={this.props.checked} name="checked" />
+          <input
+            style={{ margin: "5px 0 0 0" }}
+            className="btn btn-sm btn-primary"
+            type="submit"
+            value="Check"
+          />
+        </form>
+      );
+    }
+  }
+}
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <div className="col-6">
+        <div style={{ margin: "10px 0", padding: "10px" }} className="card">
+          <h5 className={"card-title"}>
+            {this.props.id} <TodoItemCheck checked={this.props.checked} />
+          </h5>
+
+          <p className={"card-text"}>{this.props.content}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class TodoItemCreate extends React.Component {
+  handleSubmit(event) {
+    console.log("handleSubmit");
+    return false;
+  }
+
+  render() {
+    return (
+      <form action="#" onSubmit={this.handleSubmit}>
+        <input type="hidden" value={this.props.id} name="id" />
+        <input
+          style={{ margin: "0 0 5px 0" }}
+          className="form-control"
+          type="text"
+          name="content"
+        />
+        <input type="hidden" value="false" name="checked" />
+        <input
+          style={{ margin: "5px 0 0 0" }}
+          className="btn btn-sm btn-primary"
+          type="submit"
+          value="Check"
+        />
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoItemList: [],
+      test: "hello",
+      componentList: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/items")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          todoItemList: res
+        });
+        console.log(this.state.todoItemList[0].checked);
+      });
+  }
+
+  render() {
+    let thing = this.state.todoItemList.map(item => (
+      <TodoItem
+        key={item.id}
+        id={item.id}
+        content={item.content}
+        checked={item.checked.toString()}
+      />
+    ));
+    console.log(thing);
+    return (
+      <div className={{ App }}>
+        <div className="container">
+          <div className="row">
+            <div className={"col-12"}>
+              <TodoItemCreate />
+            </div>
+            {thing}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
